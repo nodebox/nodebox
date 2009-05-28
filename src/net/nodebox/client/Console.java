86470452daa9df596d74d7dd2cae3d0990194a5d
Frederik De Bleser <frederik@pandora.be>
2009-05-28 15:03:32 +0000

    Added a bit of margin to the console + editor font.

--- src/net/nodebox/client/Console.java
+++ src/net/nodebox/client/Console.java
@@ -22,6 +22,8 @@ public class Console extends JTextPane {
 
     public Console(Pane pane) {
         this.pane = pane;
+        this.setMargin(new Insets(0, 5, 0, 5));
+        setFont(PlatformUtils.getEditorFont());
         StyleContext styles = new StyleContext();
         StyledDocument doc = new DefaultStyledDocument(styles);
         setDocument(doc);
