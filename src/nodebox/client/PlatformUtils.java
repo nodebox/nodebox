bb9bcfb05296525677054df9631eea22340cf397
Frederik De Bleser <frederik@pandora.be>
2010-01-26 15:48:08 +0100

    Find user script folder on Windows using SHGetFolderPath.
    Uses JNA to call into shell32.dll.

--- src/nodebox/client/PlatformUtils.java
+++ src/nodebox/client/PlatformUtils.java
@@ -1,5 +1,9 @@
 package nodebox.client;
 
+import com.sun.jna.*;
+import com.sun.jna.win32.W32APIFunctionMapper;
+import com.sun.jna.win32.W32APITypeMapper;
+
 import javax.swing.*;
 import java.awt.*;
 import java.io.File;
@@ -7,6 +11,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PlatformUtils {
     public static int WIN = 1;
@@ -18,19 +24,16 @@ public class PlatformUtils {
 
     public static final String SEP = System.getProperty("file.separator");
 
-    private static final String REG_QUERY_COMMAND = "reg query ";
-    private static final String REG_STRING_TOKEN = "REG_SZ";
-    private static final String REG_SHELL_FOLDERS = "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Shell Folders";
-    private static final String REG_LOCAL_APPDATA = "Local AppData";
-
     private static File userDataDirectory = null;
+    private static Map<String, Object> JNA_OPTIONS = new HashMap<String, Object>();
 
     static {
-        String osName = System.getProperty("os.name").toLowerCase();
         platformSpecificModifier = Toolkit.getDefaultToolkit().getMenuShortcutKeyMask();
-        if (osName.indexOf("windows") != -1) {
+        if (Platform.isWindows()) {
             current_platform = WIN;
-        } else if (osName.startsWith("mac os x")) {
+            JNA_OPTIONS.put(Library.OPTION_TYPE_MAPPER, W32APITypeMapper.UNICODE);
+            JNA_OPTIONS.put(Library.OPTION_FUNCTION_MAPPER, W32APIFunctionMapper.UNICODE);
+        } else if (Platform.isMac()) {
             current_platform = MAC;
         } else {
             current_platform = OTHER;
@@ -73,17 +76,20 @@ public class PlatformUtils {
         if (onMac()) {
             userDataDirectory = new File(getHomeDirectory(), "Library/" + Application.NAME);
         } else if (onWindows()) {
-            // Try to read the local application data from the system environment first.
-            // This environment variable is only available on Windows Vista/7.
-            String localAppData = System.getenv("LOCALAPPDATA");
-            if (localAppData == null) {
-                // If this fails, try to read the registry, which works on most systems, but is deprecated,
-                // and has been known to be missing.
-                localAppData = readWindowsRegistryValue(REG_SHELL_FOLDERS, REG_LOCAL_APPDATA);
-                if (localAppData == null) {
-                    // If reading the registry fails, use the home directory.
-                    localAppData = getHomeDirectory().getPath();
-                }
+            String localAppData;
+            HWND hwndOwner = null;
+            int nFolder = Shell32.CSIDL_LOCAL_APPDATA;
+            HANDLE hToken = null;
+            int dwFlags = Shell32.SHGFP_TYPE_CURRENT;
+            char[] pszPath = new char[Shell32.MAX_PATH];
+            int hResult = Shell32.INSTANCE.SHGetFolderPath(hwndOwner, nFolder, hToken, dwFlags, pszPath);
+            if (Shell32.S_OK == hResult) {
+                String path = new String(pszPath);
+                int len = path.indexOf('\0');
+                localAppData = path.substring(0, len);
+            } else {
+                // If the native call fails, use the home directory.
+                localAppData = getHomeDirectory().getPath();
             }
             userDataDirectory = new File(localAppData, Application.NAME);
         } else {
@@ -111,7 +117,7 @@ public class PlatformUtils {
     }
 
     /**
-     * Get the directory that contains Pythhon libraries the user has installed.
+     * Get the directory that contains Python libraries the user has installed.
      * <p/>
      * This directory is added to the PYTHONPATH; anything below it can be used in scripts.
      * <p/>
@@ -149,27 +155,6 @@ public class PlatformUtils {
         return KeyStroke.getKeyStroke(key, platformSpecificModifier | modifier);
     }
 
-    //// Registry ////
-
-    public static String readWindowsRegistryValue(String key, String valueKey) {
-        String command = REG_QUERY_COMMAND + " \"" + key + "\" /v \"" + valueKey + "\"";
-        try {
-            Process process = Runtime.getRuntime().exec(command);
-            StreamReader reader = new StreamReader(process.getInputStream());
-            reader.start();
-            process.waitFor();
-            reader.join();
-            String result = reader.getResult();
-            int p = result.indexOf(REG_STRING_TOKEN);
-            if (p == -1)
-                throw new RuntimeException("Cannot read Windows registry. Exiting...");
-            return result.substring(p + REG_STRING_TOKEN.length()).trim();
-        }
-        catch (Exception e) {
-            return null;
-        }
-    }
-
     public static void openURL(String url) {
         if (Desktop.isDesktopSupported()) {
             URI uri = URI.create(url);
@@ -182,29 +167,35 @@ public class PlatformUtils {
         }
     }
 
-    static private class StreamReader extends Thread {
-        private InputStream is;
-        private StringWriter sw;
-
-        StreamReader(InputStream is) {
-            this.is = is;
-            sw = new StringWriter();
-        }
-
-        public void run() {
-            try {
-                int c;
-                while ((c = is.read()) != -1)
-                    sw.write(c);
-            }
-            catch (IOException e) {
-                throw new RuntimeException("Cannot read Windows registry. Exiting...", e);
-            }
-        }
-
-        String getResult() {
-            return sw.toString();
-        }
+    private static class HANDLE extends PointerType implements NativeMapped {
     }
+
+    private static class HWND extends HANDLE {
+    }
+
+    private static interface Shell32 extends Library {
+
+        public static final int MAX_PATH = 260;
+        public static final int CSIDL_LOCAL_APPDATA = 0x001c;
+        public static final int SHGFP_TYPE_CURRENT = 0;
+        public static final int S_OK = 0;
+
+        static Shell32 INSTANCE = (Shell32) Native.loadLibrary("shell32", Shell32.class, JNA_OPTIONS);
+
+        /**
+         * See http://msdn.microsoft.com/en-us/library/bb762181(VS.85).aspx
+         * <p/>
+         *
+         * @param hwndOwner [in] Reserved.
+         * @param nFolder   [in] A CSIDL value that identifies the folder whose path is to be retrieved.
+         * @param hToken    [in] An access token that can be used to represent a particular user. Always set this to null.
+         * @param dwFlags   [in] Flags that specify the path to be returned.
+         * @param pszPath   [out] A pointer to a null-terminated string of length MAX_PATH which will receive the path.
+         * @return S_OK if successful, or an error value otherwise.
+         */
+        public int SHGetFolderPath(HWND hwndOwner, int nFolder, HANDLE hToken,
+                                   int dwFlags, char[] pszPath);
+    }
+
 
 }
