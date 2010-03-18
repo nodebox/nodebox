dfb39acadbce21cf9fe7279f311e05e844a102bb
stebanos <stefan.gabriels@gmail.com>
2010-03-18 12:04:39 +0100

    replaced old ctx method calls with the new ones.

--- src/nodebox/handle/CircleScaleHandle.java
+++ src/nodebox/handle/CircleScaleHandle.java
@@ -67,8 +67,8 @@ public class CircleScaleHandle extends AbstractHandle {
         float x = getCenterX();
         float y = getCenterY();
         float radius = getRadius();
-        ctx.setFillColor(null);
-        ctx.setStrokeColor(HANDLE_COLOR);
+        ctx.fill(null);
+        ctx.stroke(HANDLE_COLOR);
         ctx.ellipse(x, y, radius * 2, radius * 2);
         if (pt != null)
             drawDot(ctx, pt.x, pt.y);
