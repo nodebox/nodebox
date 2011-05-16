346f0195fe9e28a26ae64670e41afebf172bdb01
Frederik De Bleser <frederik@burocrazy.com>
2011-05-16 13:43:50 +0300

    If the root node returns null, also wrap it in a canvas.
    We use this so that an empty NodeBox document can also set a background color.

--- src/nodebox/node/WrapInCanvasCode.java
+++ src/nodebox/node/WrapInCanvasCode.java
@@ -7,17 +7,21 @@ public class WrapInCanvasCode implements NodeCode {
 
     public Object cook(Node node, ProcessingContext context) {
         Object o = Node.ROOT_NODE.cook(node, context);
-        if (o instanceof Geometry) {
-            Geometry geo = (Geometry) o;
+        // We also wrap null, which is the result if there are no child nodes.
+        if (o == null || o instanceof Geometry) {
             Canvas canvas = new Canvas();
             canvas.setOffsetX(node.asFloat("canvasX"));
             canvas.setOffsetY(node.asFloat("canvasY"));
             canvas.setSize(node.asFloat("canvasWidth"), node.asFloat("canvasHeight"));
             canvas.setBackground(node.asColor("canvasBackground"));
-            canvas.add(geo);
+            if (o != null) {
+                Geometry geo = (Geometry) o;
+                canvas.add(geo);
+            }
             return canvas;
-        } else
+        } else {
             return o;
+        }
     }
 
     public String getSource() {
