492834284496de74673110fc328c8ed6bb10375d
stebanos <stefan.gabriels@gmail.com>
2013-01-30 16:33:37 +0100

    receiveOSC function performs pattern matching to extract metadata about the message.

--- src/main/java/nodebox/function/DeviceFunctions.java
+++ src/main/java/nodebox/function/DeviceFunctions.java
@@ -10,7 +10,8 @@ import nodebox.node.NodeContext;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import oscP5.OscMessage;
 
@@ -47,16 +48,39 @@ public class DeviceFunctions {
         Map<String, List<Object>> oscMessages = (Map<String, List<Object>>) context.getData().get("osc.messages");
         if (oscMessages == null) return ImmutableList.of();
         if (oscAddressPrefix.isEmpty()) return ImmutableList.of();
+
+        Pattern userPattern = Pattern.compile("(<[a-z0-9-_]+>)+");
+        Matcher upMatcher = userPattern.matcher(oscAddressPrefix);
+
+        ImmutableList.Builder<String> builder = ImmutableList.builder();
+        while (upMatcher.find()) {
+            String s = upMatcher.group(0);
+            if (s.startsWith("<") && s.endsWith(">"))
+                s = s.substring(1, s.length() - 1);
+            builder.add(s);
+        }
+        ImmutableList<String> messageData = builder.build();
+
+        String convertedAddressPrefix = upMatcher.replaceAll("(XXXPLHXXX)");
+        if (! convertedAddressPrefix.endsWith("*"))
+            convertedAddressPrefix = convertedAddressPrefix + "*";
+        convertedAddressPrefix = convertedAddressPrefix.replaceAll("\\*", ".*?");
+        convertedAddressPrefix = "^" + convertedAddressPrefix.replaceAll("(XXXPLHXXX)", "[^\\/]*") + "$";
+
+        Pattern lookupPattern = Pattern.compile(convertedAddressPrefix);
         ImmutableList.Builder<Map<String, Object>> b = ImmutableList.builder();
+
         for (Map.Entry<String, List<Object>> e : oscMessages.entrySet()) {
-            if (e.getKey().startsWith(oscAddressPrefix)) {
+            Matcher lpMatcher = lookupPattern.matcher(e.getKey());
+
+            if (lpMatcher.find()) {
                 ImmutableMap.Builder<String, Object> mb = ImmutableMap.builder();
                 mb.put("address", e.getKey());
-                List<Object> values = e.getValue();
+                for (int i = 0; i < lpMatcher.groupCount(); i++)
+                    mb.put(messageData.get(i), lpMatcher.group(i + 1));
                 int i = 1;
-                for (Object o : e.getValue()) {
+                for (Object o : e.getValue())
                     mb.put("col " + i++, o);
-                }
                 b.add(mb.build());
             }
         }
