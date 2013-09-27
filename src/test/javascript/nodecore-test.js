cbab33816ac43c8887cd66f36ddd0f2c9238c480
Frederik De Bleser <frederik@burocrazy.com>
2013-09-27 11:24:01 +0200

    Further nodecore tests.

--- src/test/javascript/nodecore-test.js
+++ src/test/javascript/nodecore-test.js
@@ -142,3 +142,66 @@ test("evaluate nodes that take in lists", function () {
 
     deepEqual(nodecore.evaluateNetwork(net), [3]);
 });
+
+test("evaluate nodes that return lists and take in lists", function () {
+    var net;
+    net = {
+        name: 'root',
+        function: 'core/zero',
+        outputRange: 'LIST',
+        renderedChild: 'repeat1',
+        children: [
+            {
+                name: 'makeNumbers1',
+                function: 'math/makeNumbers',
+                outputRange: 'LIST',
+                ports: [
+                    {
+                        name: 's',
+                        type: 'string',
+                        value: '11;22;33'
+                    },
+                    {
+                        name: 'sep',
+                        type: 'string',
+                        value: ';'
+                    },
+                ]
+            },
+            {
+                name: 'repeat1',
+                function: 'list/repeat',
+                outputRange: 'LIST',
+                ports: [
+                    {
+                        name: 'list',
+                        type: 'list',
+                        range: 'LIST'
+                    },
+                    {
+                        name: 'amount',
+                        type: 'int',
+                        value: 3,
+                        range: 'VALUE'
+                    },
+                    {
+                        name: 'per_item',
+                        type: 'boolean',
+                        value: false,
+                        range: 'VALUE'
+                    }
+
+                ]
+            }
+        ],
+        connections: [
+            {
+                outputNode: 'makeNumbers1',
+                inputNode: 'repeat1',
+                inputPort: 'list'
+            }
+        ]
+    };
+
+    deepEqual(nodecore.evaluateNetwork(net), [11, 22, 33, 11, 22, 33, 11, 22, 33]);
+});
