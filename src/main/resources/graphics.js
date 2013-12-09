93fca6a70808236b8718a2ef185412caa4a3b344
stebanos <stefan.gabriels@gmail.com>
2013-12-09 16:10:12 +0100

    Added Rect type.

--- src/main/resources/graphics.js
+++ src/main/resources/graphics.js
@@ -953,27 +953,19 @@ g.bounds = function (shape) {
     return g.makeRect(0, 0, 0, 0);
 };
 
-g.makeRect = function (x, y, width, height) {
-    return { x: x, y: y, width: width, height: height };
+g.Rect = function (x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    g.deepFreeze(this);
 };
 
-g.makeRect = g.frozen(g.makeRect);
-
-g.rectContains = function (rect, x, y) {
-    return (x >= rect.x && x <= rect.x + rect.width && y >= rect.y && y <= rect.y + rect.height);
-};
-
-g.makeCenteredRect = function (cx, cy, width, height) {
-    var x = cx - width / 2,
-        y = cy - height / 2;
-    return g.makeRect(x, y, width, height);
-};
-
-g.normalizedRect = function (rect) {
-    var x = rect.x,
-        y = rect.y,
-        width = rect.width,
-        height = rect.height;
+g.Rect.prototype.normalize = function () {
+    var x = this.x,
+        y = this.y,
+        width = this.width,
+        height = this.height;
 
     if (width < 0) {
         x += width;
@@ -984,24 +976,72 @@ g.normalizedRect = function (rect) {
         y += height;
         height = -height;
     }
-    return g.makeRect(x, y, width, height);
+    return new g.Rect(x, y, width, height);
 };
 
-g.unitedRect = function (r1, r2) {
-    r1 = g.normalizedRect(r1);
-    r2 = g.normalizedRect(r2);
-
-    var x = Math.min(r1.x, r2.x),
-        y = Math.min(r1.y, r2.y),
-        width = Math.max(r1.x + r1.width, r2.x + r2.width) - x,
-        height = Math.max(r1.y + r1.height, r2.y + r2.height) - y;
-
-    return g.makeRect(x, y, width, height);
-
+g.Rect.prototype.containsPoint = function (x, y) {
+    return (x >= this.x && x <= this.x + this.width && y >= this.y && y <= this.y + this.height);
 };
 
-g.getRectCentroid = function (rect) {
-    return g.makePoint(rect.x + rect.width / 2,  rect.y + rect.height / 2);
+g.Rect.prototype.containsRect = function (r) {
+    return r.x >= this.x && r.x + r.width <= this.x + this.width &&
+        r.y >= this.y && r.y + r.height <= this.y + this.height;
+};
+
+g.Rect.prototype.grow = function (dx, dy) {
+    var x = this.x - dx,
+        y = this.y - dy,
+        width = this.width + dx * 2,
+        height = this.height + dy * 2;
+    return new g.Rect(x, y, width, height);
+};
+
+g.Rect.prototype.unite = function (r) {
+    var x = Math.min(this.x, r.x),
+        y = Math.min(this.y, r.y),
+        width = Math.max(this.x + this.width, r.x + r.width) - x,
+        height = Math.max(this.y + this.height, r.y + r.height) - y;
+    return new g.Rect(x, y, width, height);
+};
+
+g.Rect.prototype.addPoint = function (x, y) {
+    var dx, dy,
+        _x = this.x,
+        _y = this.y,
+        width = this.width,
+        height = this.height;
+
+    if (x < this.x) {
+        dx = this.x - x;
+        _x = x;
+        width += dx;
+    } else if (x > this.x + this.width) {
+        dx = x - (this.x + this.width);
+        width += dx;
+    }
+    if (y < this.y) {
+        dy = this.y - y;
+        _y = y;
+        height += dy;
+    } else if (y > this.y + this.height) {
+        dy = y - (this.y + this.height);
+        height += dy;
+    }
+    return new g.Rect(_x, _y, width, height);
+};
+
+g.Rect.prototype.centroid = function () {
+    return new g.Point(this.x + this.width / 2, this.y + this.height / 2);
+};
+
+g.makeRect = function (x, y, width, height) {
+    return new Rect(x, y, width, height);
+};
+
+g.makeCenteredRect = function (cx, cy, width, height) {
+    var x = cx - width / 2,
+        y = cy - height / 2;
+    return new g.Rect(x, y, width, height);
 };
 
 g.RGB = "RGB";
