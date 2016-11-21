5ef7cf12dfd2eb31e5492dd0b6dc7ddbc393b624
rambutan2000 <geordiemoffatt@rocketmail.com>
2016-11-21 12:28:16 -0800

    Adding test python echo server for websockets integration

---
+++ src/test/python/server.py
@@ -0,0 +1,31 @@
+from websocket_server import WebsocketServer
+import json
+
+# Called for every client connecting (after handshake)
+def new_client(client, server):
+	print("New client connected and was given id %d" % client['id'])
+	server.send_message_to_all("Hey all, a new client has joined us")
+
+
+# Called for every client disconnecting
+def client_left(client, server):
+	print("Client(%d) disconnected" % client['id'])
+
+
+# Called when a client sends a message
+def message_received(client, server, message):
+	jsonData = json.loads(message)
+	idStr = jsonData["id"]
+	print(jsonData["msg"])
+	jsonData["msg"] += "_SERVER"
+
+	server.send_message_to_all(json.dumps(jsonData))
+
+
+
+PORT=9001
+server = WebsocketServer(PORT)
+server.set_fn_new_client(new_client)
+server.set_fn_client_left(client_left)
+server.set_fn_message_received(message_received)
+server.run_forever()
