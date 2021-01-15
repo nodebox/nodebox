62eab3d98cc9fb322f6d35551bf10e0132226825
Frederik De Bleser <frederik@debleser.be>
2021-01-15 09:36:47 +0100

    Fix more deprecation warnings.

--- src/test/java/nodebox/graphics/Playground.java
+++ src/test/java/nodebox/graphics/Playground.java
@@ -8,6 +8,7 @@ import java.awt.*;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import java.io.StringReader;
 
@@ -124,7 +125,7 @@ public class Playground extends JFrame {
     private class RunAction extends AbstractAction {
         private RunAction() {
             super("Run");
-            putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_R, Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx()));
+            putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_R, InputEvent.META_DOWN_MASK));
         }
 
         @Override
