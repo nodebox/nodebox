8921338894621e8bec0b2037e71506ffc3a5e7b1
Frederik De Bleser <frederik@debleser.be>
2025-05-19 15:13:55 +0200

    Update example to use custom URLs

--- react-embed-example/src/app.jsx
+++ react-embed-example/src/app.jsx
@@ -9,7 +9,14 @@ function App() {
   return (
     <main className="app__main">
       <h1 className="app__title">NodeBox Player Embed Demo</h1>
-      <NodeBoxPlayer userId="example" projectId="scatter" item="" values={values} />
+      <NodeBoxPlayer
+        userId="example"
+        projectId="scatter"
+        item=""
+        values={values}
+        publishedUrlTemplate="https://nodeboxtest.s3.amazonaws.com/{{ userId }}/{{ projectId }}/versions/published.json"
+        assetsUrlTemplate="https://nodeboxtest.s3.amazonaws.com/{{ userId }}/{{ projectId }}/blobs/{{ hash }}"
+      />
     </main>
   );
 }
